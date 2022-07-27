a5ae9fd9f18c6c552264b71174f21e619efee0e7
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-07-27 15:09:40 +0200

    apply contraint to get words (adjective, noun,...)

--- server/controllers/v1/words.js
+++ server/controllers/v1/words.js
@@ -1,8 +1,27 @@
 const data = require('../../data/TestData.json');
 
+const checkContains = ( words ) => {
+  const wordConstraints = {adjective:0, noun:0, verb:0, adverb:0};
+  const wordConstraintsKeys = Object.keys(wordConstraints);
+  words.map(word => {
+    if (wordConstraintsKeys.includes(word.pos)) {
+      wordConstraints[word.pos] += 1;
+    }
+  });
+
+  wordConstraintsKeys.forEach(key => {
+    if (wordConstraints[key] < 1) {
+      return false;
+    }
+  })
+  return true;
+}
+
 const getRandomWords = ( words, number ) => {
   const shuffled = words.sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, number);
+  const slice = shuffled.slice(0, number);
+
+  return checkContains(slice) ? slice : getRandomWords(words, number);
 }
 
 module.exports = {
